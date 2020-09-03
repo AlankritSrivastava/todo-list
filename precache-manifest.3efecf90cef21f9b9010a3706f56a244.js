9aa0da79c6428f7b1ef993307892a83d1057739b
AlankritSrivastava <alankritsri2000@gmail.com>
2020-09-03 19:38:57 +0530

    Updates

---
+++ precache-manifest.3efecf90cef21f9b9010a3706f56a244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c8b11314e136b3264b17540a85a2b7",
+    "url": "/AlankritSrivastava/todo-list.git/index.html"
+  },
+  {
+    "revision": "fecd92819106878e70ad",
+    "url": "/AlankritSrivastava/todo-list.git/static/css/main.ccb42480.chunk.css"
+  },
+  {
+    "revision": "c8d11292ad57a8826f84",
+    "url": "/AlankritSrivastava/todo-list.git/static/js/2.2a7e33b9.chunk.js"
+  },
+  {
+    "revision": "fea17e90e7ccac1a017279c0975d8084",
+    "url": "/AlankritSrivastava/todo-list.git/static/js/2.2a7e33b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecd92819106878e70ad",
+    "url": "/AlankritSrivastava/todo-list.git/static/js/main.8999d128.chunk.js"
+  },
+  {
+    "revision": "f82606bd82a31cc23277",
+    "url": "/AlankritSrivastava/todo-list.git/static/js/runtime-main.93bca8c5.js"
+  }
+]);
